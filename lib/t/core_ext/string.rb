7791b335f2bfe664bb68b9086274bf0df5213234
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-12-27 16:22:56 +0100

    Added methods for colorizing output

--- lib/t/core_ext/string.rb
+++ lib/t/core_ext/string.rb
@@ -12,4 +12,54 @@ class String
   def to_i(base = 10)
     tr(',', '').old_to_i(base)
   end
+
+  def c(*codes)
+    codes = codes.flatten.map { |code|
+      case code
+      when String, Symbol
+        T::Printable::COLOR[code.to_sym] rescue nil
+      else
+        code
+      end
+    }.compact.unshift(0)
+    "\e[#{codes.join(';')}m#{self}\e[0m"
+  end
+
+  def coloring(pattern, color = nil, &block)
+    self.gsub(pattern) do |i|
+      applied_colors = $`.scan(/\e\[[\d;]+m/)
+      c = color || block.call(i)
+      "#{i.c(c)}#{applied_colors.join}"
+    end
+  end
+
+  t = {
+    ?& => "&amp;",
+    ?< => "&lt;",
+    ?> => "&gt;",
+    ?' => "&apos;",
+    ?" => "&quot;",
+  }
+
+  define_method(:u) do
+    gsub(/(#{Regexp.union(t.values)})/o, t.invert)
+  end
+
+  define_method(:e) do
+    gsub(/[#{t.keys.join}]/o, t)
+  end
+
+  def indent(count, char = ' ')
+    (char * count) + gsub(/(\n+)/) { |m| m + (char * count) }
+  end
+
+  def trim_indent
+    lines = self.split("\n")
+    unindent = self.split("\n").select { |s| s !~ /^\s$/ }.map { |s| s.index(/[^\s]/) || 0 }.min
+    lines.map { |s| s.gsub(/^#{' ' * unindent}/, '') }.join("\n")
+  end
+
+  #def colorize(color_code)
+  #  "\e[#{color_code}m#{self}\e[0m"
+  #end
 end
